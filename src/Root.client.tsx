c3bbe0d62335f9465bdaab0a40e24c7a99f0e53c
dhythm <yuta.okada.20@gmail.com>
2023-03-12 16:07:52 +0900

    wip

--- src/Root.client.tsx
+++ src/Root.client.tsx
@@ -1,9 +1,9 @@
-// // @ts-expect-error
-// import { FC, PropsWithChildren, use, useState } from "react";
-// // @ts-expect-error
-// import { createFromFetch } from "react-server-dom-webpack/client";
+// @ts-expect-error
+import { FC, PropsWithChildren, use, useState } from "react";
+// @ts-expect-error
+import { createFromFetch } from "react-server-dom-webpack/client";
 
-import { FC, PropsWithChildren, useState } from "react";
+// import { FC, PropsWithChildren, useState } from "react";
 import { LocationContext, Location } from "./LocationContext.client";
 
 export const Root: FC<PropsWithChildren> = ({ children }) => {
@@ -20,16 +20,16 @@ export const Root: FC<PropsWithChildren> = ({ children }) => {
   );
 };
 
-// const dataFromServer = `
-// 1:"$Sreact.suspense"
-// 0:["$","div",null,{"className":"main","children":[["$","section",null,{"className":"col sidebar","children":[["$","section",null,{"className":"sidebar-header","children":["$","strong",null,{"children":"React + Prisma"}]}],["$","section",null,{"className":"sidebar-menu","role":"menubar"}],["$","nav",null,{"children":["$","$1",null,{"fallback":"Loading...","children":"Suspensed"}]}]]}],["$","section","null",{"className":"col viewer","children":["$","$1",null,{"fallback":"Loading...","children":"Suspensed"}]}]]}]
-// `;
-// // FIXME: Webpack is crashed if importing createFromFetch...
-// const chunk = createFromFetch(
-//   fetch(`data:text/plain;base64,${btoa(dataFromServer)}`)
-//   // fetch(`/rsc`)
-// );
-//
-// const Container: FC = () => {
-//   return use(chunk);
-// };
+const dataFromServer = `
+1:"$Sreact.suspense"
+0:["$","div",null,{"className":"main","children":[["$","section",null,{"className":"col sidebar","children":[["$","section",null,{"className":"sidebar-header","children":["$","strong",null,{"children":"React + Prisma"}]}],["$","section",null,{"className":"sidebar-menu","role":"menubar"}],["$","nav",null,{"children":["$","$1",null,{"fallback":"Loading...","children":"Suspensed"}]}]]}],["$","section","null",{"className":"col viewer","children":["$","$1",null,{"fallback":"Loading...","children":"Suspensed"}]}]]}]
+`;
+// FIXME: Webpack is crashed if importing createFromFetch...
+const chunk = createFromFetch(
+  fetch(`data:text/plain;base64,${btoa(dataFromServer)}`)
+  // fetch(`/rsc`)
+);
+
+const Container: FC = () => {
+  return use(chunk);
+};
